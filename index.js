3b7bcca15d01eaace26bace24b7ed3d7bad8f83c
guigabytez <ahg0406@gmail.com>
2021-09-22 18:56:32 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+const axios = require('axios')
+const Telegram = require('node-telegram-bot-api')
+
+const bot = new Telegram(process.env.TELEGRAM_API)
+
+weatherApi = process.env.WEATHER_API
+
+const weatherUrl = new URL("https://api.openweathermap.org/data/2.5/weather")
+weatherUrl.searchParams.set('q','Cebu City, Philippines')
+weatherUrl.searchParams.set('appid', weatherApi)
+weatherUrl.searchParams.set('units', 'imperial')
+
+const getWeatherData = async () => {
+    const response = await axios.get(weatherUrl.toString())
+    const data = response.data
+    return data
+}
+
+const generateWeatherMessage = weatherData => {
+    const { name, weather, main, temp_min, temp_max } = weatherData
+    return `The weather in ${name}: ${weather[0].description}. Current temperature is ${main.temp}, with a low temp of ${main.temp_min} and a high of ${main.temp_max}.`
+}
+
+const app = async () => {
+    try {
+        const weatherData = await getWeatherData()
+        const weatherString = generateWeatherMessage(weatherData)
+        bot.sendMessage(process.env.TELEGRAM_CHAT_ID, weatherString)
+    } catch (error) {
+        throw new Error(error)
+    }
+    
+    
+}
+
+app()
